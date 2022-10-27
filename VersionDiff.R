854adf391210474ffdc4b778fed397f7df2bfd08
Dean Adams <dcadams@iastate.edu>
2022-10-27 16:46:51 +0200

    ms 3-DCA

--- VersionDiff.R
+++ VersionDiff.R
@@ -3,5 +3,5 @@
     # NOTE: need to install latexdiffr from: https://github.com/hughjonesd/latexdiffr
 
 #Run latex diff by hand in MS directory
-shell(cmd="latexdiff PristurusMS-2-DCA.tex PristurusMS-3.tex > MS-diff-2-3.tex")
-tinytex::latexmk("MS-diff-2-3.tex") 
+shell(cmd="latexdiff PristurusMS-3.tex PristurusMS-2-DCA.tex > MS-diff-2DCA-3.tex")
+tinytex::latexmk("MS-diff-2DCA-3.tex") 
