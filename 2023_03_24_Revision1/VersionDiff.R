26925bc98bd25879837dc8af564e592a25e98ad5
Dean Adams <dcadams@iastate.edu>
2023-04-15 08:22:08 +0200

    v2

--- 2023_03_24_Revision1/VersionDiff.R
+++ 2023_03_24_Revision1/VersionDiff.R
@@ -3,5 +3,5 @@
     # NOTE: need to install latexdiffr from: https://github.com/hughjonesd/latexdiffr
 
 #Run latex diff by hand in MS directory
-shell(cmd="latexdiff PristurusMS-Final.tex PristurusMS-R1.tex > MS-diff.tex")
+shell(cmd="latexdiff PristurusMS-Final.tex PristurusMS-R1_2.tex > MS-diff.tex")
 tinytex::latexmk("MS-diff.tex") 
