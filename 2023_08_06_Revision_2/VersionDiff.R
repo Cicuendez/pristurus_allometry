050b0a9393355c6ad5269455b07a2c4980c107a5
Dean Adams <dcadams@iastate.edu>
2023-08-14 18:06:15 -0500

    vsn 2 revision

--- 2023_08_06_Revision_2/VersionDiff.R
+++ 2023_08_06_Revision_2/VersionDiff.R
@@ -3,6 +3,6 @@
     # NOTE: need to install latexdiffr from: https://github.com/hughjonesd/latexdiffr
 
 #Run latex diff by hand in MS directory
-shell(cmd="latexdiff R1_previous_version/PristurusMS-R1_2.tex PristurusMS-R2.tex > TrackChanges.tex")
-tinytex::latexmk("TrackChanges.tex") 
+shell(cmd="latexdiff PristurusMS-R1-text.tex PristurusMS-R2-DCA-text.tex > TrackChangesText.tex")
+tinytex::latexmk("TrackChangesText.tex") 
 
